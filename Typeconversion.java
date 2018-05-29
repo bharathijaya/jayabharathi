ecd456fc93525c2ac0743527de4ce7936104dcec
bharathijaya <jayabharathi9897@gmail.com>
2018-05-28 22:50:25 -0700

    HelloWorld

---
+++ Typeconversion.java
@@ -0,0 +1,11 @@
+public class TypeConversion
+{
+ public static void main(String args[])
+{
+ System.out.println("welcome to java training");
+}
+ byte one = 50;
+ byte two = 50;
+ byte three = byte(one + two);
+ System.out.println("three");
+}
